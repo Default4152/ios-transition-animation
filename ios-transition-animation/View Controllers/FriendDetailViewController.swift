93e461dc778fe20cf089c2de00d55d190aef29ee
Conner Alegre <cga0@pm.me>
2018-08-30 13:59:11 -0600

    FriendDetailViewController base

---
+++ ios-transition-animation/View Controllers/FriendDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FriendDetailViewController.swift
+//  ios-transition-animation
+//
+//  Created by Conner on 8/30/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class FriendDetailViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+}

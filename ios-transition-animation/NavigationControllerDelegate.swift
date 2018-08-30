1f144e1b1dda2aab61c09aaf1bc1aa83b9fca617
Conner Alegre <cga0@pm.me>
2018-08-30 14:25:26 -0600

    add NavigationControllerDelegate base

---
+++ ios-transition-animation/NavigationControllerDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationControllerDelegate.swift
+//  ios-transition-animation
+//
+//  Created by Conner on 8/30/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class NavigationControllerDelegate: NSObject, UINavigationControllerDelegate {
+//  func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+//  }
+  
+  var sourceCell: UITableViewCell?
+}
